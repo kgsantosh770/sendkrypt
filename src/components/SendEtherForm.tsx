f6aad8b696e68945e44ca07f45639950e062da53
Santosh <s.kunga.ganeshbabu@accenture.com>
2022-10-30 22:47:58 +0530

    send ether complete functionality without test

--- src/components/SendEtherForm.tsx
+++ src/components/SendEtherForm.tsx
@@ -1,17 +1,82 @@
-import { KeyboardEvent } from "react";
+import { ChangeEvent, KeyboardEvent, useState } from "react";
+import sendEther from "../features/send-ethers/SendEther";
+
+interface IFormData {
+  receiver: string,
+  amount: string,
+  keyword: string,
+  msg: string,
+}
 
 export default function SendEtherForm() {
+  const [formData, setFormData] = useState<IFormData>({
+    receiver: '',
+    amount: '',
+    keyword: '',
+    msg: '',
+  });
+
   const blockInvalidChar = (e: KeyboardEvent) =>
     ['e', 'E', '+', '-'].includes(e.key) && e.preventDefault();
 
+  const handleSubmit = () => {
+    //form validation
+    const amount = Number(formData.amount);
+    sendEther(formData.receiver, amount, formData.keyword, formData.msg);
+  }
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => {
+      return {
+        ...prevData,
+        [name]: value,
+      }
+    })
+  }
+
   return (
-    <form className="mt-6 mb-16 lg:my-6 bg-customblue-200 py-6 px-4 rounded-2xl max-w-md mx-auto">
-      <input type="text" name="receiver" className="bg-customblue-100 mb-5 block w-full shadow-black shadow-sm rounded-lg min-h-max px-5 py-2.5" placeholder="Address to" />
-      <input type="number" name="amount" max="10000" onKeyDown={blockInvalidChar} className="bg-[#252849] mb-5 block w-full shadow-black shadow-sm rounded-lg min-h-max px-5 py-2.5" placeholder="Ether" />
-      <input type="text" name="keyword" className="bg-customblue-100 mb-5 block w-full shadow-black shadow-sm rounded-lg min-h-max px-5 py-2.5" placeholder="Keyword" />
-      <input type="text" name="message" className="bg-customblue-100 mb-5 block w-full shadow-black shadow-sm rounded-lg min-h-max px-5 py-2.5" placeholder="Message" />
+    <form className="mt-6 mb-16 lg:my-6 bg-customblue-200 py-6 px-4 rounded-2xl max-w-md mx-auto" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="receiver"
+        value={formData.receiver}
+        onChange={handleInputChange}
+        className="bg-customblue-100 mb-5 block w-full shadow-black shadow-sm rounded-lg min-h-max px-5 py-2.5"
+        placeholder="Address to" />
+      <input
+        type="number"
+        name="amount"
+        value={formData.amount}
+        onChange={handleInputChange}
+        max="10000"
+        onKeyDown={blockInvalidChar}
+        className="bg-[#252849] mb-5 block w-full shadow-black shadow-sm rounded-lg min-h-max px-5 py-2.5"
+        placeholder="Ether"
+      />
+      <input
+        type="text"
+        name="keyword"
+        value={formData.keyword}
+        onChange={handleInputChange}
+        className="bg-customblue-100 mb-5 block w-full shadow-black shadow-sm rounded-lg min-h-max px-5 py-2.5"
+        placeholder="Keyword"
+      />
+      <input
+        type="text"
+        name="message"
+        value={formData.msg}
+        onChange={handleInputChange}
+        className="bg-customblue-100 mb-5 block w-full shadow-black shadow-sm rounded-lg min-h-max px-5 py-2.5"
+        placeholder="Message"
+      />
       <hr className="bg-white mx-4 my-3" />
-      <input type="submit" value="Send" className="bg-customblue-100 mt-5 w-1/2 block mx-auto border-white border shadow-black shadow-md py-3 px-3 rounded-xl" />
+      <input
+        type="submit"
+        value="Send"
+        onChange={handleInputChange}
+        className="bg-customblue-100 mt-5 w-1/2 block mx-auto border-white border shadow-black shadow-md py-3 px-3 rounded-xl"
+      />
     </form>
   )
 }
--- src/features/send-ethers/SendEther.ts
+++ src/features/send-ethers/SendEther.ts
@@ -1,9 +1,9 @@
-import { abi } from "./abi.json";
+import contractData from "./abi.json";
 import { ethereum } from '../../utils/Constants';
 import { ethers } from "ethers";
 
 const CONTRACT_ADDRESS = '0x5CB150e65a803948b297C536c2f1eF229432738f';
-const CONTRACT_ABI = abi;
+const CONTRACT_ABI = contractData.abi;
 
 const getContract = () => {
     if (!ethereum) return false;
@@ -12,11 +12,12 @@ const getContract = () => {
     return new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
 }
 
-const sendEther = async (to: string, keyword: string, msg: string) => {
+const sendEther = async (to: string, amount: number, keyword: string, msg: string) => {
+    const weiAmount = ethers.utils.parseEther(amount.toString());
     try {
         const contract = getContract();
         if (!contract) return false;
-        const txn = await contract.sendEther(to, keyword, msg);
+        const txn = await contract.sendEther(to, keyword, msg, {value: weiAmount});
         await txn.wait();
         return true;
     } catch (error) {
