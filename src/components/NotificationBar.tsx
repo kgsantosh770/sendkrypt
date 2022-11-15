458f5435ac292a049cc7489cb39da4d09dafb08a
Santosh <s.kunga.ganeshbabu@accenture.com>
2022-11-15 15:36:13 +0530

    removed close button

--- src/components/NotificationBar.tsx
+++ src/components/NotificationBar.tsx
@@ -6,19 +6,14 @@ interface NotificationBarProps {
 }
 
 export default function NotificationBar(props: NotificationBarProps) {
-    const [showNotification, setShowNotification] = useState(props.showNotification);
-    if (showNotification) {
-        return (
-            <div className="bg-white py-4 px-10 relative">
-                {props.message}
-                <button
-                    onClick={()=>setShowNotification(true)}
-                    className="absolute right-10 pb-2 rounded-full bg-customblue-200 w-7 h-7 text-white font-semibold"
-                >
-                    x
-                </button>
-            </div>
-        )
-    }
-    return null;
+    return (
+        <>
+            {
+                props.showNotification &&
+                <div className="bg-white py-4 px-10 relative">
+                    {props.message}
+                </div>
+            }
+        </>
+    )
 }
